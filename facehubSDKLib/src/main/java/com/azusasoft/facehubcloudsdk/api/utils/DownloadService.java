2d8cdfce45d1a20e540985d56f21cb2be29b3363
Seta <sweater@azusasoft.com>
2016-04-14 18:03:14 +0800

    清理下载代码

--- facehubSDKLib/src/main/java/com/azusasoft/facehubcloudsdk/api/utils/DownloadService.java
+++ facehubSDKLib/src/main/java/com/azusasoft/facehubcloudsdk/api/utils/DownloadService.java
@@ -56,15 +56,6 @@ public class DownloadService {
      * @param resultHandler 下载回调
      */
     public static void download(String url,final File dir , final String path, final ResultHandlerInterface resultHandler){
-//        DownloadTask task = new DownloadTask(url,dir,path,resultHandler);
-//        Future future = ThreadPoolManager.getDownloadThreadPool().submit( task );
-//        try {
-//            future.get();
-//        } catch (InterruptedException | ExecutionException e) {
-//            LogX.e("下载出错 : " + e);
-//        }
-        /** ======================================================================= */
-
         File file0 = new File(dir.getAbsolutePath().concat(path));
         if(file0.exists()){
 //            fastLog("图片已下载,不重复下载");
@@ -125,104 +116,5 @@ public class DownloadService {
         });
     }
 
-    static class DownloadTask implements Runnable{
-        private String url,path;
-        private File dir;
-        private ResultHandlerInterface resultHandler;
 
-        public DownloadTask(String url,final File dir , final String path, final ResultHandlerInterface resultHandler){
-            this.url = url;
-            this.dir = dir;
-            this.path = path;
-            client.setMaxConnections(5);
-            this.resultHandler = new ResultHandlerInterface() {
-                @Override
-                public void onResponse(final Object response) {
-                    handler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            resultHandler.onResponse(response);
-                        }
-                    });
-                }
-
-                @Override
-                public void onError(final Exception e) {
-                    handler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            resultHandler.onError(e);
-                        }
-                    });
-                }
-            };
-        }
-
-        @Override
-        public void run() {
-            File file0 = new File(dir.getAbsolutePath().concat(path));
-            if(file0.exists()){
-//            fastLog("图片已下载,不重复下载");
-                resultHandler.onResponse(file0);
-                return;
-            }
-            if(url==null){
-                LogX.e("Image url null !!");
-                resultHandler.onError(new Exception("Image url null !!"));
-                return;
-            }
-            if(!checkDir(dir)){
-                resultHandler.onError(new Exception("Fail to create download directory."));
-                return;
-            }
-//        fastLog("开始下载");
-
-            handler.post(new Runnable() {
-                @Override
-                public void run() {
-                    RequestHandle download = client.get(url, new BinaryHttpResponseHandler() {
-                        //        RequestHandle download = new AsyncHttpClient().get(url, new BinaryHttpResponseHandler() {
-                        @Override
-                        public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, byte[] binaryData) {
-                            OutputStream f = null;
-                            try {
-
-                                File file = new File(dir.getAbsolutePath().concat(path));
-                                f = new FileOutputStream(file);
-                                f.write(binaryData); //your bytes
-                                f.close();
-
-                                resultHandler.onResponse(file);
-                            } catch (FileNotFoundException e) {
-                                e.printStackTrace();
-                                resultHandler.onError(e);
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                                resultHandler.onError(e);
-                            }
-
-                        }
-
-                        @Override
-                        public String[] getAllowedContentTypes() {
-                            String[] oldTypes = super.getAllowedContentTypes();
-                            String[] types = new String[oldTypes.length + 1];
-                            for (int i = 0; i < oldTypes.length; ++i) {
-                                types[i] = oldTypes[i];
-                            }
-                            types[types.length - 1] = "text/plain";
-                            return types;
-                        }
-
-                        @Override
-                        public void onFailure(int statusCode, cz.msebera.android.httpclient.Header[] headers, byte[] binaryData, Throwable error) {
-                            LogX.e("facehub_cloud", "download err : " + error);
-                            resultHandler.onError(new Exception(error));
-                        }
-
-                    });
-                }
-            });
-        }
-    }
 }
