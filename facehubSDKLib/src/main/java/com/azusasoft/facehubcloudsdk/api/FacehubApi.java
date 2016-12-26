53451f06b4c7e0b3b3a604c63d7048415ab33358
Seta <sweater@azusasoft.com>
2016-12-26 14:47:01 +0800

    src->tools:src

--- facehubSDKLib/src/main/java/com/azusasoft/facehubcloudsdk/api/FacehubApi.java
+++ facehubSDKLib/src/main/java/com/azusasoft/facehubcloudsdk/api/FacehubApi.java
@@ -64,6 +64,7 @@ import static com.azusasoft.facehubcloudsdk.api.utils.UtilMethods.parseHttpError
  */
 public class FacehubApi implements CacheApiInterface{
     final static String HOST = "https://yun.facehub.me";  //外网
+//    final static String HOST = "http://test.facehub.me:9292";
 //            protected final static String HOST = "http://106.75.15.179:9292";  //测服
     //        protected final static String HOST = "http://10.0.0.79:9292";  //内网
 
